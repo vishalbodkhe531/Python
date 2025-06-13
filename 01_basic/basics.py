303e21fad3575ffc1dcd762d402962cd930d251a
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-06-13 12:26:32 +0530

    add age program

--- 01_basic/basics.py
+++ 01_basic/basics.py
@@ -3,3 +3,23 @@ def printWord(arg):
     print(arg)
 
 printWord("Hello world")    
+
+
+
+# Predict The Age 
+
+ageInput = int(input("Enter your age : "));
+
+print(input);
+
+if ageInput < 20 :
+    print("less than 20");
+
+elif(ageInput < 30) : 
+    print("less than 30");
+
+elif(ageInput < 40) : 
+    print("less than 40");
+
+else : 
+    print("Age is expire");
