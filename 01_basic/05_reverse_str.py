500fbd87175edbb9230e403ee05311f8d85d079d
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-06-14 09:26:01 +0530

    add reverse string program

---
+++ 01_basic/05_reverse_str.py
@@ -0,0 +1,16 @@
+input = "Hello, I am Vishal"
+reversed_string1 = ""
+
+for char in input:
+    reversed_string1 = char + reversed_string1;
+
+print(reversed_string1)
+
+
+# using slicing 
+
+newInput = "Hello, I am Vishal"
+
+reversed_string2 = newInput[::-1];
+
+print(reversed_string2)
