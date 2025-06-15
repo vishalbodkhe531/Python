4dd76be29f4aa79d03293a76eb955043f350b041
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-06-15 18:36:00 +0530

    add 2 function programs

---
+++ 01_basic/15_multiple_para.py
@@ -0,0 +1,7 @@
+
+def sumAll(*args):
+    print(sum(args))
+    return sum(args)
+
+sumAll(1,2,3)
+sumAll(1,2,3,4,5)
---
+++ 01_basic/16_sequence_arg.py
@@ -0,0 +1,5 @@
+def application(user,email):
+    print(f"User : {user} \nEmail {email}")
+
+ #We can pass like this also   
+application(email="ram@gmail.com",user="Ram")
