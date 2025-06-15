3ed41dc3368dbf92f0054e8ca8625bc257394a17
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-06-15 17:33:57 +0530

    add 2 programs

---
+++ 01_basic/13_default_parameter.py
@@ -0,0 +1,7 @@
+
+
+def greet(name = "ram"):
+    print(f"Hello {name}")
+
+greet("Pavan")
+greet()
---
+++ 01_basic/14_lambda_function.py
@@ -0,0 +1,6 @@
+# lambda or anonymous function
+
+cube = lambda no : no ** 3
+
+print(cube)
+print(cube(2))
